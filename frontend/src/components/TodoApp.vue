3ab260b2ce0f1e5e2c08ddf63d3faf6c7db9af20
Trinmar Boado <timi.boado@gmail.com>
2022-07-03 15:36:08 +0800

    html and css

---
+++ frontend/src/components/TodoApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>Hello Batch 41</h4>
+    <h4 class="blue-color backg-black">Hello Batch 42</h4>
+    <input type="text" placeholder="What needs to be done?">
+    <ul>
+      <li>asdasdasdasd</li>
+      <li>qwe</li>
+      <li>zxc</li>
+      <li>ghj</li>
+      <li>asdasdasdasd</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+h4 {
+  color: pink;
+}
+.blue-color {
+  color:aqua;
+}
+.backg-black {
+  background-color:black;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,10 +2,15 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    component: () => import('components/TodoApp.vue')
+    // component: () => import('layouts/MainLayout.vue'),
+    // children: [
+    //   { path: '', component: () => import('pages/IndexPage.vue') }
+    // ]
+  },
+  {
+    path: '/about',
+    component: () => import('components/AnotherPage.vue')
   },
 
   // Always leave this as last one,
