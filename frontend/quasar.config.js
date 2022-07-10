802394ef0c01d75f2a190630da00a36f7d10edc7
Trinmar Boado <timi.boado@gmail.com>
2022-07-10 15:58:05 +0800

    directory path

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -59,14 +59,14 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash' // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/batch-41'
       // analyze: true,
       // env: {},
       // rawDefine: {}
